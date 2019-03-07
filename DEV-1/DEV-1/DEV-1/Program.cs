686ae341ac7e258bb64cb5ba90ae0c0732d2981f
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-03-07 19:05:31 +0300

    Implement method of getting max sequence

--- DEV-1/DEV-1/DEV-1/Program.cs
+++ DEV-1/DEV-1/DEV-1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DEV_1
 {
@@ -10,6 +6,11 @@ namespace DEV_1
     {
         static void Main(string[] args)
         {
+            StringConverter stringConverter = new StringConverter();
+            foreach(var value in stringConverter.GetMaxUniqueSequences("access"))
+            {
+                Console.WriteLine(value);
+            }           
         }
     }
 }
---
+++ DEV-1/DEV-1/DEV-1/StringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DEV_1
+{
+    public class StringConverter
+    {
+        public List<string> GetMaxUniqueSequences(string receivedString)
+        {
+            StringBuilder maxUniqueSequence = new StringBuilder();
+            List<string> allMaxSequence = new List<string>();
+            for (int i = 0; i < receivedString.Length - 1; i++)
+            {
+                maxUniqueSequence.Append(receivedString[i]);
+                if (receivedString[i] == receivedString[i + 1])
+                {
+                    allMaxSequence.Add(maxUniqueSequence.ToString());
+                    maxUniqueSequence.Clear();
+                }
+            }
+            if (maxUniqueSequence.Length != 0)
+            {
+                maxUniqueSequence.Append(receivedString[receivedString.Length - 1]);
+                allMaxSequence.Add(maxUniqueSequence.ToString());
+            }
+            allMaxSequence.RemoveAll(s => s.Length == 1);
+            return allMaxSequence;
+        }
+    }
+}
