aec8cc997a02c8a785674986ef5fd84917eaf2ff
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-03-07 20:01:13 +0300

    Update GetMaxUniqueSequence method

---
+++ DEV-1/DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DEV_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            var stringConverter = new StringConverter();
+            var maxUniqueSequences = stringConverter.GetMaxUniqueSequences("accesserasdfgassfgadd");
+            foreach (var value in maxUniqueSequences)
+            {
+                Console.WriteLine(value);
+            }
+            Console.Read();
+        }
+    }
+}
