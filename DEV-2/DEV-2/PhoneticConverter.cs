fbf21e1cac4f8cdec02631405cc9ba41be86a396
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-03-16 20:31:30 +0300

    add method of converting sharp and vowel letters

--- DEV-2/DEV-2/PhoneticConverter.cs
+++ DEV-2/DEV-2/PhoneticConverter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace DEV_2
 {
@@ -7,6 +9,7 @@ namespace DEV_2
         public string GetPhoneticRepresentation(string receivedString)
         {
             receivedString = ReplaceOSymbolsWithouAccent(receivedString);
+            receivedString = ReplaceSonantAndSharp(receivedString);
             return receivedString;
         }
         private string ReplaceOSymbolsWithouAccent(string receivedString)
@@ -16,5 +19,49 @@ namespace DEV_2
                             .Where(e => e != '+')
                             .ToArray());
         }
+        private string ReplaceSonantAndSharp(string receivedString)
+        {
+            var result = new StringBuilder(receivedString);
+            if (SonantToVowelDictionary.ContainsKey(result[result.Length-1]))
+            {
+                result[result.Length - 1] = SonantToVowelDictionary[result[result.Length - 1]];
+            }
+            for (var i = result.Length - 1; i > 0; i--)
+            {
+                if (SonantToVowelDictionary.ContainsValue(result[i]) && SonantToVowelDictionary.ContainsKey(result[i - 1]))
+                {
+                    result[i - 1] = SonantToVowelDictionary[result[i - 1]];                  
+                }
+                if (VowelToSonantDictionary.ContainsValue(result[i]) &&  VowelToSonantDictionary.ContainsKey(result[i - 1]))
+                {
+                    result[i - 1] = VowelToSonantDictionary[result[i - 1]];
+                }
+            }
+            return result.ToString();
+        }
+        private readonly Dictionary<char, char> SonantToVowelDictionary = new Dictionary<char, char>()
+        {
+            ['б'] = 'п',
+            ['в'] = 'ф',
+            ['д'] = 'т',
+            ['з'] = 'с',
+            ['ж'] = 'ш',
+            ['г'] = 'к',
+            ['х'] = 'х',
+            ['ц'] = 'ц',
+            ['ч'] = 'ч',
+            ['щ'] = 'щ',
+        };
+
+        private readonly Dictionary<char, char> VowelToSonantDictionary = new Dictionary<char, char>()
+        {
+            ['п'] = 'б',
+            ['ф'] = 'в',
+            ['т'] = 'д',
+            ['с'] = 'з',
+            ['ш'] = 'ж',
+            ['к'] = 'г',
+        };
+
     }
 }
