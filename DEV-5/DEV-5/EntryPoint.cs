a947ec09d5ce0a88514e1836cf5e29d1a9f0c528
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-03-30 02:37:33 +0300

    implement getdistance method

--- DEV-5/DEV-5/EntryPoint.cs
+++ DEV-5/DEV-5/EntryPoint.cs
@@ -1,9 +1,14 @@
-﻿namespace DEV_5
+﻿using System;
+using Points;
+
+namespace DEV_5
 {
     public class EntryPoint
     {
         public static void Main(string[] args)
         {
+            var point = new Point(0,0,0);
+            Console.WriteLine(point.GetAbsoluteDistanceToPoint(1, 2, 3));
         }
     }
 }
--- DEV-5/Points/Point.cs
+++ DEV-5/Points/Point.cs
@@ -1,4 +1,6 @@
-﻿namespace Points
+﻿using static System.Math;
+
+namespace Points
 {
     public struct Point
     {
@@ -13,6 +15,18 @@
             XCoordinate = xCoordinate;
             YCoordinate = yCoordinate;
             ZCoordinate = zCoordinate;
+        }
+
+        public double GetAbsoluteDistanceToPoint(Point targetPoint)
+        {
+            return Sqrt(Pow(targetPoint.XCoordinate - XCoordinate, 2) + Pow(targetPoint.YCoordinate - YCoordinate, 2) +
+                        Pow(targetPoint.ZCoordinate - ZCoordinate, 2));
+        }
+
+        public double GetAbsoluteDistanceToPoint(int xCoordinate, int yCoordinate, int zCoordinate)
+        {
+            return Sqrt(Pow(xCoordinate - XCoordinate, 2) + Pow(yCoordinate - YCoordinate, 2) +
+                        Pow(zCoordinate - ZCoordinate, 2));
         }
     }
 }
