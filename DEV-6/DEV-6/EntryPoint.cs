b2ac8f4470d189c91ad6f24c58ea79efcb8d6e6c
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-04-10 17:28:19 +0300

    implement few methods

--- DEV-6/DEV-6/EntryPoint.cs
+++ DEV-6/DEV-6/EntryPoint.cs
@@ -1,9 +1,19 @@
-﻿namespace DEV_6
+﻿using System;
+
+namespace DEV_6
 {
     public class EntryPoint
     {
         public static void Main(string[] args)
         {
+            var s = new XmlCarParser();
+
+            var sas = s.GetGarFromDocument("Cars.xml");
+
+            foreach (var ers in sas)
+            {
+                Console.WriteLine(ers.Brand);
+            }
         }
     }
 }
--- DEV-6/DEV-6/XMLCarParser.cs
+++ DEV-6/DEV-6/XMLCarParser.cs
@@ -1,7 +1,68 @@
-﻿namespace DEV_6
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace DEV_6
 {
-    public class XMLCarParser
+    public class XmlCarParser
     {
-        
+        private readonly List<Car> _listCars;
+
+        public XmlCarParser()
+        {
+            _listCars = new List<Car>();
+        }
+
+        public List<Car> GetGarFromDocument(string path)
+        {
+            _listCars.Clear();
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(path);
+            var xmlRoot = xmlDocument.DocumentElement;
+
+            foreach (XmlNode xmlNode in xmlRoot ?? throw new ArgumentException("XML file is empty"))
+            {
+                _listCars.Add(GetCar(xmlNode));
+            }
+
+
+            return _listCars;
+        }
+
+        private Car GetCar(XmlNode xmlNode)
+        {
+            var brand = string.Empty;
+            var model = string.Empty;
+            var amount = 0;
+            var price = 0.0;
+
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                try
+                {
+                    switch (childNode.Name)
+                    {
+                        case "brand":
+                            brand = childNode.InnerText;
+                            break;
+                        case "model":
+                            model = childNode.InnerText;
+                            break;
+                        case "amount":
+                            amount = int.Parse(childNode.InnerText);
+                            break;
+                        case "price":
+                            price = double.Parse(childNode.InnerText);
+                            break;
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    throw new ArgumentException("Wrong data in XML file");
+                }               
+            }
+
+            return new Car(brand, model, amount, price);
+        }
     }
 }
