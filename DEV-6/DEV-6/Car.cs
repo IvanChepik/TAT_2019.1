c5ee89f7e67702b76c58b60f8373858a5b2c69c1
Ivan Chepik <barbudas.barbudass@gmail.com>
2019-04-10 13:32:05 +0300

    small fix

--- DEV-6/DEV-6/Car.cs
+++ DEV-6/DEV-6/Car.cs
@@ -1,7 +1,28 @@
-﻿namespace DEV_6
+﻿using System;
+
+namespace DEV_6
 {
     public class Car
     {
-        
+        public string Brand { get;   }
+
+        public string Model { get;  }
+
+        public int Amount { get;  }
+
+        public double Price { get; }
+
+        public Car(string brand, string model, int amount, double price)
+        {
+            if ((brand == string.Empty) || (model == string.Empty) || (amount < -1) || (price <= 0))
+            {
+                throw new ArgumentException("Wrong argument.");
+            }
+
+            Brand = brand;
+            Model = model;
+            Amount = amount;
+            Price = price;
+        }
     }
 }
